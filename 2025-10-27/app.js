43e026a3b84613aa5615d6c6628247c65744b0bd
StaffanBaloo <staffan.johansson@gmail.com>
2025-10-27 17:24:19 +0100

    uppdatera med if-else i st f ? :

--- 2025-10-27/app.js
+++ 2025-10-27/app.js
@@ -1,5 +1,5 @@
-let age = 65;
-let kategori =
+let age = 2;
+/* let kategori =
     age < 13
         ? "ett barn"
         : age < 18
@@ -9,9 +9,22 @@ let kategori =
         : age < 65
         ? "vuxen"
         : "pensionär";
-
-if (age < 0 || isNaN(age)) {
+ */
+/* if (age < 0 || isNaN(age)) {
     console.log("Ogiltig ålder!");
 } else {
     console.log(`En person som är ${age} år är ${kategori}.`);
+} */
+if (age < 0 || isNaN(age)) {
+    console.log("Ogiltig ålder!");
+} else if (age < 13) {
+    console.log(`En person som är ${age} år är ett barn.`);
+} else if (age < 18) {
+    console.log(`En person som är ${age} år är en tonåring.`);
+} else if (age < 20) {
+    console.log(`En person som är ${age} år är en tonårig vuxen.`);
+} else if (age < 65) {
+    console.log(`En person som är ${age} år är vuxen.`);
+} else {
+    console.log(`En person som är ${age} år är pensionär.`);
 }
